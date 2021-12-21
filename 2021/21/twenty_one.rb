e98aa47ffde837a04795fe76c6a9b67a0d99d628
Cathal Coffey <coffey.cathal@gmail.com>
2021-12-21 22:06:01 +0000

    More verbose but more clear

--- 2021/21/twenty_one.rb
+++ 2021/21/twenty_one.rb
@@ -14,24 +14,29 @@ class TwentyOne
     return p2_score * roll if p1_score >= 1000
     return p1_score * roll if p2_score >= 1000
 
-    # Player 1 moves
-    new_p1_pos = (p1_pos + roll + 1 + roll + 2 + roll + 3) % 10
-    new_p1_score = p1_score + new_p1_pos + 1
+    if (roll % 2).zero? # Player 1's move
+      new_p1_pos = (p1_pos + roll + 1 + roll + 2 + roll + 3) % 10
+      new_p1_score = p1_score + new_p1_pos + 1
 
-    # Player 2 goes next
-    play_part_1(p1_pos: p2_pos, p1_score: p2_score, p2_pos: new_p1_pos, p2_score: new_p1_score, roll: roll + 3)
+      play_part_1(p1_pos: new_p1_pos, p1_score: new_p1_score, p2_pos: p2_pos, p2_score: p2_score, roll: roll + 3)
+    else # Player 2's move
+      new_p2_pos = (p2_pos + roll + 1 + roll + 2 + roll + 3) % 10
+      new_p2_score = p2_score + new_p2_pos + 1
+
+      play_part_1(p1_pos: p1_pos, p1_score: p1_score, p2_pos: new_p2_pos, p2_score: new_p2_score, roll: roll + 3)
+    end
   end
 
   def part_2(player_1_position:, player_2_position:)
     # Board positions 0..9 are much more convenient to work with than 1..10
     p1_pos = player_1_position == 1 ? 10 : player_1_position - 1
     p2_pos = player_2_position == 1 ? 10 : player_2_position - 1
-    play_part_2(p1_pos: p1_pos, p1_score: 0, p2_pos: p2_pos, p2_score: 0).max
+    play_part_2(p1_pos: p1_pos, p1_score: 0, p2_pos: p2_pos, p2_score: 0, turn: 0).max
   end
 
-  def play_part_2(p1_pos:, p1_score:, p2_pos:, p2_score:, memo: {})
+  def play_part_2(p1_pos:, p1_score:, p2_pos:, p2_score:, turn:, memo: {})
     # Memoize
-    key = [p1_pos, p1_score, p2_pos, p2_score]
+    key = [p1_pos, p1_score, p2_pos, p2_score, turn]
     return memo[key] if memo.key?(key)
 
     # Base cases
@@ -41,26 +46,33 @@ class TwentyOne
     # All possible sums of 3 dice throws
     all_possible_rolls = [1, 2, 3].repeated_permutation(3).map(&:sum)
 
-    p1_wins = 0
-    p2_wins = 0
-    all_possible_rolls.each do |roll|
-      # Player 1 moves
-      new_p1_pos = (p1_pos + roll) % 10
-      new_p1_score = p1_score + new_p1_pos + 1
-
-      # Player 2 goes next
-      new_p2_wins, new_p1_wins = play_part_2(
-        p1_pos: p2_pos,
-        p1_score: p2_score,
-        p2_pos: new_p1_pos,
-        p2_score: new_p1_score,
-        memo: memo
-      )
-
-      p1_wins += new_p1_wins
-      p2_wins += new_p2_wins
+    results = all_possible_rolls.map do |roll|
+      if (turn % 2).zero? # Player 1's move
+        new_p1_pos = (p1_pos + roll) % 10
+        new_p1_score = p1_score + new_p1_pos + 1
+        play_part_2(
+          p1_pos: new_p1_pos,
+          p1_score: new_p1_score,
+          p2_pos: p2_pos,
+          p2_score: p2_score,
+          turn: turn + 1,
+          memo: memo
+        )
+      else # Player 2's move
+        new_p2_pos = (p2_pos + roll) % 10
+        new_p2_score = p2_score + new_p2_pos + 1
+        play_part_2(
+          p1_pos: p1_pos,
+          p1_score: p1_score,
+          p2_pos: new_p2_pos,
+          p2_score: new_p2_score,
+          turn: turn + 1,
+          memo: memo
+        )
+      end
     end
 
+    p1_wins, p2_wins = results.reduce { |a, b| [a[0] + b[0], a[1] + b[1]] }
     memo[key] = [p1_wins, p2_wins]
     memo[key]
   end
